37dd4bbb72a2d4d1ff76c25e6f76b83a26c9b5d5
NCSchmidt <nicklas.c.schmidt@gmail.com>
2018-09-07 10:32:07 -0700

    campaign updates

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
     console.log( "ready!" );
 
-    var campaignNames = ["Chrome","Chromebook US","Chromebook CA","GCP DR","POM","Chrome Enterprise Security","G Suite DR","G Suite Consideration","Express DR Q3","KOF H2","Google Maps Brand","Google Duo US Brand","YouTube Music DR","YouTube Premium DR","YouTube TV DR","YouTube Artists Brand","Google Home Mini US:","Google Home Max US:","Google Home CA:","Google Home QB:","Domains","G Suite","GCP","Chrome Enterprise","Adwords DR","Adwords Youtube DR","Google Express","Google My Business","GA Free","Chrome Win 10","Chromebook US","Chromebook CA","Project Fi Brand","Nest NA Q3 2018 North America - Nest.com Evergreen DR","[Nest EMEA Q3 2018 EMEA - Consumer DR]","Google Store NA Q3 2018 North America - DR","Pixelbook NA Q2-Q3 2018 North America - DR","Nest NA Q3 2018 North America - Nest.com Evergreen DR","[Nest EMEA Q3 2018 EMEA - Consumer DR]","Play Store NA Q3 2018 - United States","Play Store NA Q3 2018 - Canada","YouTube TV DR Q3 2018","YouTube Music DR Q2-Q4 2018","Project Fi Q3 2018","Google Assistant Q3 2018","Pixel NA '18 Q3 2018 United States","Pixel NA '18 Q3 2018 Canada","Pixel Q3 US","Google Store Q3","Home Mini BD Q3","Pixelbook Q3 US","Play Evergreen","Play Orson","Brand Editorial","Nest Pro B2B H2 2018"];
+    var campaignNames = ["Chrome","Chromebook US","Chromebook CA","GCP DR","POM","Chrome Enterprise Security","G Suite DR","G Suite Consideration","Express DR Q3","KOF H2","Google Maps Brand","Google Duo US Brand","YouTube Music DR","YouTube Premium DR","YouTube TV DR","YouTube Artists Brand","Google Home Mini US:","Google Home Max US:","Google Home CA:","Google Home QB:","Domains","G Suite","GCP","Chrome Enterprise","Adwords DR","Adwords Youtube DR","Google Express","Google My Business","GA Free","Chrome Win 10","Chromebook US","Chromebook CA","Project Fi Brand","Nest NA Q3 2018 North America - Nest.com Evergreen DR","[Nest EMEA Q3 2018 EMEA - Consumer DR]","Google Store NA Q3 2018 North America - DR","Pixelbook NA Q2-Q3 2018 North America - DR","Nest NA Q3 2018 North America - Nest.com Evergreen DR","[Nest EMEA Q3 2018 EMEA - Consumer DR]","Play Store NA Q3 2018 - United States","Play Store NA Q3 2018 - Canada","YouTube TV DR Q3 2018","YouTube Music DR Q2-Q4 2018","Project Fi Q3 2018","Google Assistant Q3 2018","Pixel NA '18 Q3 2018 United States","Pixel NA '18 Q3 2018 Canada","Pixel Q3 US","Google Store Q3","Home Mini BD Q3","Pixelbook Q3 US","Play Evergreen","Play Orson","Brand Editorial","Nest Pro B2B H2 2018","Chromebook US Q3"];
     var platformNames = ["DV3","YouTube","DV360","Google Preferred","Custom Algo","Platform: YouTube","Platform: DV360","Google SEM","GDN","Bing SEM","Google SEM:","Bing SEM:","GDN:","Yahoo SEM:","Yahoo SEM","Yahoo Native:","Yahoo Native","TrueView","Bing","Yahoo","Presto - Bing","GMB - Bing","Google GDN","[Google SEM]","[Yahoo SEM]","[Bing SEM]","[GDN]","[Google Ads SEM]","GDN Cases","GDN US & CA","Google SEM US","Google SEM CA","Google Cases","GDN In-App","GDN RMKT+DCO","GDN Live Cases","Google SEM - DMA","Google SEM - Music","Google SEM - GPM","Google SEM - Premium","Yahoo SEM - Music","Google SEM - YTO","UAC - Music","Google SEM - PLAs","-Google SEM","YT BR+DR","DV360 DR","YT Brand","Spotify"];
 
     $("#submitButton").on("click",function() {
@@ -16,6 +16,10 @@ $(document).ready(function() {
         var inputTextArrayByLine = inputText.split(/\n/);
 
         var outputArray = [];
+
+        // show campaigns for QA
+        var outputCampaignArray = [];
+        var outputCampaignArrayUnique = [];
 
         for (var n=0; n < inputTextArrayByLine.length; n++) {
             var currentLine = inputTextArrayByLine[n].trim();
@@ -32,6 +36,7 @@ $(document).ready(function() {
                 }
             }
 
+            
             // adds in platform and campaign if it's a platform
             for (var p=0; p < platformNames.length; p++) {
                 var platform = platformNames[p];
@@ -41,8 +46,20 @@ $(document).ready(function() {
                     var fixedCampaign = activeCampaign.replace(/:|\[|\]/gi, "");
                     outputArray.push(" ",fixedCampaign,fixedPlatform);
                     console.log("added!");
+
+                    // show campaigns for QA
+                    outputCampaignArray.push(fixedCampaign);
+                    console.log(outputCampaignArray);
+                    
                 }
             }
+
+            function onlyUnique(value, index, self) { 
+                return self.indexOf(value) === index;
+            }
+            outputCampaignArrayUnique = outputCampaignArray.filter( onlyUnique );
+            $("#outputCampaigns").html("<p><strong>" + outputCampaignArrayUnique.length + " campaigns</strong></p>" + "<p>" + outputCampaignArrayUnique.join("\n") + "</p>");
+
 
             // find colons and push rest of characters to outputArray
             var colonFound = currentLine.indexOf(":");
