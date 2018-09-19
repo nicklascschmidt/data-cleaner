2658295d84a6e4143451573c889e0afb634c6722
NCSchmidt <nicklas.c.schmidt@gmail.com>
2018-09-19 11:25:22 -0700

    camp plat updates

--- javascript.js
+++ javascript.js
@@ -16,14 +16,17 @@ $(document).ready(function() {
                         "Play Store NA Q3 2018 - United States","Play Store NA Q3 2018 - Canada","YouTube TV DR Q3 2018",
                         "YouTube Music DR Q2-Q4 2018","Project Fi Q3 2018","Google Assistant Q3 2018","Pixel NA '18 Q3 2018 United States",
                         "Pixel NA '18 Q3 2018 Canada","Pixel Q3 US","Google Store Q3","Home Mini BD Q3","Pixelbook Q3 US","Play Evergreen",
-                        "Play Orson","Brand Editorial","Nest Pro B2B H2 2018","Chromebook US Q3","Orson","Nest EMEA Q3 2018 EMEA - Consumer DR"];
+                        "Play Orson","Brand Editorial","Nest Pro B2B H2 2018","Chromebook US Q3","Orson","Nest EMEA Q3 2018 EMEA - Consumer DR",
+                        "Pixel NA '18 Q3 2018 US","Chrome DR","Chrome Brand (Birthday Campaign)"];
     var platformNames = ["DV3","YouTube","DV360","Google Preferred","Custom Algo","Platform: YouTube","Platform: DV360","Google SEM","GDN",
                         "Bing SEM","Google SEM:","Bing SEM:","GDN:","Yahoo SEM:","Yahoo SEM","Yahoo Native:","Yahoo Native","TrueView",
                         "Bing","Yahoo","Presto - Bing","GMB - Bing","Google GDN","[Google SEM]","[Yahoo SEM]","[Bing SEM]","[GDN]",
                         "[Google Ads SEM]","GDN Cases","GDN US & CA","Google SEM US","Google SEM CA","Google Cases","GDN In-App",
                         "GDN RMKT+DCO","GDN Live Cases","Google SEM - DMA","Google SEM - Music","Google SEM - GPM","Google SEM - Premium",
                         "Yahoo SEM - Music","Google SEM - YTO","UAC - Music","Google SEM - PLAs","-Google SEM","YT BR+DR","DV360 DR",
-                        "YT Brand","Spotify","DV360 Laptop Mag PG Deal","YT Brand","Spotify DV360"];
+                        "YT Brand","Spotify","DV360 Laptop Mag PG Deal","YT Brand","Spotify DV360","DV360 - Princesses and Moana Pre-Roll",
+                        "YT Brand  - 15MM, 40MM and Even Flighting Scenarios","YT Brand  - Burst","Yahoo Display","YouTube Music Scale Brand",
+                        "Bing SEM - Music"];
 
     $("#submitButton").on("click",function() {
         $("#output").empty();
