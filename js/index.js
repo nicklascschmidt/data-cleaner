6b93e237b82ca5425c79c15d5b1b55a1bad73b24
NCSchmidt <nicklas.c.schmidt@gmail.com>
2018-09-24 10:38:28 -0700

    data.js restructure

--- js/index.js
+++ js/index.js
@@ -11,19 +11,7 @@ $(document).ready(function() {
         campaignNames = JSON.parse(localStorage.getItem("campaignNames"));
     } else {
         console.log('campaignNames doesnt exist yet, populating now...');
-        campaignNames = ["Chrome","Chromebook US","Chromebook CA","GCP DR","POM","Chrome Enterprise Security","G Suite DR",
-            "G Suite Consideration","Express DR Q3","KOF H2","Google Maps Brand","Google Duo US Brand","YouTube Music DR",
-            "YouTube Premium DR","YouTube TV DR","YouTube Artists Brand","Google Home Mini US:","Google Home Max US:",
-            "Google Home CA:","Google Home QB:","Domains","G Suite","GCP","Chrome Enterprise","Adwords DR","Adwords Youtube DR",
-            "Google Express","Google My Business","GA Free","Chrome Win 10","Chromebook US","Chromebook CA","Project Fi Brand",
-            "Nest NA Q3 2018 North America - Nest.com Evergreen DR","[Nest EMEA Q3 2018 EMEA - Consumer DR]",
-            "Google Store NA Q3 2018 North America - DR","Pixelbook NA Q2-Q3 2018 North America - DR",
-            "Nest NA Q3 2018 North America - Nest.com Evergreen DR","[Nest EMEA Q3 2018 EMEA - Consumer DR]",
-            "Play Store NA Q3 2018 - United States","Play Store NA Q3 2018 - Canada","YouTube TV DR Q3 2018",
-            "YouTube Music DR Q2-Q4 2018","Project Fi Q3 2018","Google Assistant Q3 2018","Pixel NA '18 Q3 2018 United States",
-            "Pixel NA '18 Q3 2018 Canada","Pixel Q3 US","Google Store Q3","Home Mini BD Q3","Pixelbook Q3 US","Play Evergreen",
-            "Play Orson","Brand Editorial","Nest Pro B2B H2 2018","Chromebook US Q3","Orson","Nest EMEA Q3 2018 EMEA - Consumer DR",
-            "Pixel NA '18 Q3 2018 US","Chrome DR","Chrome Brand (Birthday Campaign)"];
+        campaignNames = campaignNameArray; // populated from ./data.js
 
         localStorage.setItem("campaignNames", JSON.stringify(campaignNames));
 
@@ -35,21 +23,12 @@ $(document).ready(function() {
         platformNames = JSON.parse(localStorage.getItem("platformNames"));
     } else {
         console.log('platformNames doesnt exist yet, populating now...');
-        platformNames = ["DV3","YouTube","DV360","Google Preferred","Custom Algo","Platform: YouTube","Platform: DV360","Google SEM","GDN",
-            "Bing SEM","Google SEM:","Bing SEM:","GDN:","Yahoo SEM:","Yahoo SEM","Yahoo Native:","Yahoo Native","TrueView",
-            "Bing","Yahoo","Presto - Bing","GMB - Bing","Google GDN","[Google SEM]","[Yahoo SEM]","[Bing SEM]","[GDN]",
-            "[Google Ads SEM]","GDN Cases","GDN US & CA","Google SEM US","Google SEM CA","Google Cases","GDN In-App",
-            "GDN RMKT+DCO","GDN Live Cases","Google SEM - DMA","Google SEM - Music","Google SEM - GPM","Google SEM - Premium",
-            "Yahoo SEM - Music","Google SEM - YTO","UAC - Music","Google SEM - PLAs","-Google SEM","YT BR+DR","DV360 DR",
-            "YT Brand","Spotify","DV360 Laptop Mag PG Deal","YT Brand","Spotify DV360","DV360 - Princesses and Moana Pre-Roll",
-            "YT Brand  - 15MM, 40MM and Even Flighting Scenarios","YT Brand  - Burst","Yahoo Display","YouTube Music Scale Brand",
-            "Bing SEM - Music"];
 
         localStorage.setItem("platformNames", JSON.stringify(platformNames));
     }
 
 
-    // add campaigns and store
+    // add campaigns and store to local storage
     $('#submitButtonCampaign').on('click',function() {
         var campaignInput = $('#input-campaign').val().trim();
         var campaignInputArray = campaignInput.split('\n');
@@ -65,7 +44,7 @@ $(document).ready(function() {
         $('#input-campaign').val('');
     });
 
-    // add platforms and store
+    // add platforms and store to local storage
     $('#submitButtonPlatform').on('click',function() {
         var platformInput = $('#input-platform').val().trim();
         var platformInputArray = platformInput.split('\n');
